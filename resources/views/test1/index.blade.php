35cbca6d216b8441ddcd8b392c4cba0a44584b63
Fujita <manabu.fjt@gmail.com>
2023-12-09 15:13:02 +0900

    気圧差があるときのみ表示するように変更

--- resources/views/test1/index.blade.php
+++ resources/views/test1/index.blade.php
@@ -170,7 +170,7 @@
 
                                 <div class="text-center">
                                     <div class="border m-2 py-3 border-2 border-secondary-emphasis rounded-3">
-                                    <div class="row mb-3">
+                                    <div class="row">
                                             <div class="col">
                                             @if ($weather['weather1'] == "Clear")  <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" style="fill:orange"><path d="M361.5 1.2c5 2.1 8.6 6.6 9.6 11.9L391 121l107.9 19.8c5.3 1 9.8 4.6 11.9 9.6s1.5 10.7-1.6 15.2L446.9 256l62.3 90.3c3.1 4.5 3.7 10.2 1.6 15.2s-6.6 8.6-11.9 9.6L391 391 371.1 498.9c-1 5.3-4.6 9.8-9.6 11.9s-10.7 1.5-15.2-1.6L256 446.9l-90.3 62.3c-4.5 3.1-10.2 3.7-15.2 1.6s-8.6-6.6-9.6-11.9L121 391 13.1 371.1c-5.3-1-9.8-4.6-11.9-9.6s-1.5-10.7 1.6-15.2L65.1 256 2.8 165.7c-3.1-4.5-3.7-10.2-1.6-15.2s6.6-8.6 11.9-9.6L121 121 140.9 13.1c1-5.3 4.6-9.8 9.6-11.9s10.7-1.5 15.2 1.6L256 65.1 346.3 2.8c4.5-3.1 10.2-3.7 15.2-1.6zM160 256a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zm224 0a128 128 0 1 0 -256 0 128 128 0 1 0 256 0z"/></svg> @endif
                                             @if ($weather['weather1'] == "Clouds") <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512" style="fill:gray"><path d="M0 336c0 79.5 64.5 144 144 144H512c70.7 0 128-57.3 128-128c0-61.9-44-113.6-102.4-125.4c4.1-10.7 6.4-22.4 6.4-34.6c0-53-43-96-96-96c-19.7 0-38.1 6-53.3 16.2C367 64.2 315.3 32 256 32C167.6 32 96 103.6 96 192c0 2.7 .1 5.4 .2 8.1C40.2 219.8 0 273.2 0 336z"/></svg> @endif
@@ -184,7 +184,16 @@
                                             </div>
                                     </div>
 
-                                    <div class="row">
+                                    <!-- 空行 -->
+                                    @if ($weather['pressure_diff'] > 0 
+                                        || $weather['wind_int'] > 2 
+                                        || $weather['rain'] > 0)
+                                             <p></p>
+                                    @endif
+
+                                    <!-- 気圧差 -->
+                                    @if ($weather['pressure_diff'] > 0)
+                                    <div class="row mt-3">
                                         <div class="col">
                                             @if ($weather['pressure_diff'] > 0)
                                             &plus;
@@ -192,7 +201,9 @@
                                             {{ $weather['pressure_diff'] }} hPa
                                         </div>
                                     </div>
+                                    @endif
 
+                                    <!-- 風速 -->
                                     @if ($weather['wind_int'] > 2)
                                     <div class="row">
                                         <div class="col">
@@ -202,6 +213,7 @@
                                     </div>
                                     @endif
 
+                                    <!-- 降水量 -->
                                     @if ($weather['rain'] > 0)
                                     <div class="row">
                                         <div class="col">
